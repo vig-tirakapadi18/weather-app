24549727d9648fb897b1e914fde380f50207969a
vig-titakapadi18 <vighneshvt2000@gmail.com>
2024-06-18 16:18:49 +0530

    displayed the weather data on UI

--- src/components/WeatherDetails.jsx
+++ src/components/WeatherDetails.jsx
@@ -1,5 +1,19 @@
-const WeatherDetails = () => {
-  return <div>WeatherDetails</div>;
+const WeatherDetails = ({ weatherDetails }) => {
+  console.log(weatherDetails.weather[0].description);
+  return (
+    <>
+      {weatherDetails && (
+        <div className="weather-details">
+          <h2>
+            Weather Details of <span>{weatherDetails.name}</span>
+          </h2>
+          <h1>{weatherDetails.main.temp}</h1>
+          <h3>{weatherDetails.weather[0].description}</h3>
+          <p>Wind Speed: {weatherDetails.wind.speed}</p>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default WeatherDetails;
