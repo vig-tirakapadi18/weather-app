1b09eb933c7cb217b169e19f82e0623dd2880dd2
vig-titakapadi18 <vighneshvt2000@gmail.com>
2024-06-18 19:15:52 +0530

    styled the project using tailwindCSS

--- src/components/GetWeather.jsx
+++ src/components/GetWeather.jsx
@@ -1,35 +1,48 @@
-import { Fragment } from "react";
 import { useState } from "react";
 import WeatherDetails from "./WeatherDetails";
 
 const GetWeather = () => {
   const [city, setCity] = useState("");
   const [weatherDetails, setWeatherDetails] = useState("");
+  const [error, setError] = useState("");
 
   const apiKey = "8e07810dc6743ea3994df51b510a2aa9";
 
   const clickHandler = async (event) => {
     event.preventDefault();
-    const fetchWeatherResponse = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
-    );
-    const fetchWeatherResponseData = await fetchWeatherResponse.json();
-    setWeatherDetails(fetchWeatherResponseData);
+    try {
+      const fetchWeatherResponse = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+      );
+      const fetchWeatherResponseData = await fetchWeatherResponse.json();
+      setWeatherDetails(fetchWeatherResponseData);
+    } catch (error) {
+      console.log(error.message);
+      setError(error.message);
+    }
   };
 
   return (
     <>
-      <form>
+      <form className="flex flex-col h-[30vh] justify-center items-center gap-2">
         <input
           type="text"
           placeholder="Enter city name"
           value={city}
           onChange={(event) => setCity(event.target.value)}
+          className="p-1 rounded-sm  w-[60%] outline-none"
         />
-        <button onClick={clickHandler}>Get Weather</button>
+        <button
+          onClick={clickHandler}
+          className="text-white bg-emerald-600  w-[30%] p-1 rounded-[5rem] cursor-pointer active:scale-95 ">
+          Get Weather
+        </button>
       </form>
 
-      <WeatherDetails weatherDetails={weatherDetails} />
+      <WeatherDetails
+        weatherDetails={weatherDetails}
+        error={error}
+      />
     </>
   );
 };
--- src/components/WeatherDetails.jsx
+++ src/components/WeatherDetails.jsx
@@ -1,16 +1,26 @@
-const WeatherDetails = ({ weatherDetails }) => {
-  console.log(weatherDetails.weather[0].description);
+const WeatherDetails = ({ weatherDetails, error }) => {
   return (
     <>
-      {weatherDetails && (
-        <div className="weather-details">
-          <h2>
-            Weather Details of <span>{weatherDetails.name}</span>
+      {weatherDetails ? (
+        <div className="mx-[20%] bg-stone-300 p-4 rounded-lg text-center">
+          <h2 className="uppercase font-bold text-2xl">
+            {weatherDetails.name}
           </h2>
-          <h1>{weatherDetails.main.temp}</h1>
-          <h3>{weatherDetails.weather[0].description}</h3>
-          <p>Wind Speed: {weatherDetails.wind.speed}</p>
+          <h1 className="text-5xl text-center font-bold">
+            {weatherDetails.main.temp} <span className="text-3xl">°C</span>
+          </h1>
+          <h3 className="uppercase font-semibold">
+            {weatherDetails.weather[0].description}
+          </h3>
+          <p>
+            Wind Speed:{" "}
+            <span className="text-2xl font-semibold">
+              {weatherDetails.wind.speed} m/s
+            </span>
+          </p>
         </div>
+      ) : (
+        <p className="text-rose-500 text-center">{error}</p>
       )}
     </>
   );
